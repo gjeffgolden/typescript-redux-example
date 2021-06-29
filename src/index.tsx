ed5c9deb5b6bcc1e6fba9c57a155bee701c201c3
gjeffgolden <gjgolden08@gmail.com>
2021-06-29 08:55:55 -0600

    Scaffolding Redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
+import { App } from './components/App'
+import { reducers } from './reducers/index'
 
-interface AppProps {
-    color?: string
-}
+const store = createStore(reducers, applyMiddleware(thunk))
 
+// interface AppProps {
+//     color?: string
+// }
+
+//FUNCTIONAL COMPONENT:
 // const App = (props: AppProps): JSX.Element => {
 //     return <div>{props.color}</div>
 // }
 
-class App extends React.Component<AppProps> {
-    state = { counter: 0 }
+//CLASS COMPONENT:
+// class App extends React.Component<AppProps> {
+//     state = { counter: 0 }
 
-    onIncrement = (): void => {
-        this.setState({ counter: this.state.counter + 1 })
-    }
+//     onIncrement = (): void => {
+//         this.setState({ counter: this.state.counter + 1 })
+//     }
 
-    onDecrement = (): void => {
-        this.setState({ counter: this.state.counter - 1 })
-    }
+//     onDecrement = (): void => {
+//         this.setState({ counter: this.state.counter - 1 })
+//     }
 
-    render() {
-        return(
-            <div>
-                <button onClick={this.onIncrement}>Increment</button>
-                <button onClick={this.onDecrement}>Decrement</button>
-                {this.state.counter}
-            </div>
-        )
-    }
-}
+//     render() {
+//         return(
+//             <div>
+//                 <button onClick={this.onIncrement}>Increment</button>
+//                 <button onClick={this.onDecrement}>Decrement</button>
+//                 {this.state.counter}
+//             </div>
+//         )
+//     }
+// }
 
 ReactDOM.render(
-    <App color="red"/>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
